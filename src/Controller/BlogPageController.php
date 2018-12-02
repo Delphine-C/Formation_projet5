8f5404ce3b11018872683b2bccdc3c35fadc9597
Delphine-C <corneil.delphine@gmail.com>
2018-12-02 20:41:10 +0100

    Get articles from DB for blog

--- src/Controller/BlogPageController.php
+++ src/Controller/BlogPageController.php
@@ -10,14 +10,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\ArticleCRUD;
 
 class BlogPageController extends Controller
 {
     /**
      * @Route("/blog",name="blog_page")
      */
-    public function blog()
+    public function blog(ArticleCRUD $CRUD)
     {
+        $articles = $CRUD->getArticles();
+        //return $this->render('blog.html.twig', ['articles' => $articles]);
         return $this->render('blog.html.twig');
     }
 }
