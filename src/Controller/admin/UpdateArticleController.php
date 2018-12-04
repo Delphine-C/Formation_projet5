bd86a3b1ea43b7ef00df5aeacb15cec05407f0e8
Delphine-C <corneil.delphine@gmail.com>
2018-12-04 17:12:07 +0100

    Update an article

---
+++ src/Controller/admin/UpdateArticleController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Delphine_Corneil
+ * Date: 04/12/2018
+ * Time: 16:58
+ */
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ArticleType;
+use App\Service\ArticleCRUD;
+use Symfony\Component\HttpFoundation\Request;
+
+class UpdateArticleController extends Controller
+{
+    /**
+     * @Route("/modifier-article/{id}",name="update_article")
+     */
+    public function updateArticle($id, ArticleCRUD $articleCRUD, Request $request)
+    {
+        $article = $articleCRUD->getArticle($id);
+        $form = $this->createForm(ArticleType::class, $article)
+            ->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $articleCRUD->saveArticle($article);
+
+            return $this->redirectToRoute("blog_page");
+        }
+
+        return $this->render('admin/article.html.twig', ['form' => $form->createView()]);
+    }
+}
