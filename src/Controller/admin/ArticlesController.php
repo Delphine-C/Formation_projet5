3948765523fe863b7f7b2cbe3ed8ddc45090c14e
Benoit <benoit.lefevre22@gmail.com>
2018-12-05 13:30:08 +0100

    page article created

---
+++ src/Controller/admin/ArticlesController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Delphine_Corneil
+ * Date: 21/11/2018
+ * Time: 16:34
+ */
+
+namespace App\Controller\admin;
+
+use App\Entity\Article;
+use App\Form\ConnexionType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticlesController extends Controller
+{
+    /**
+     * @Route("/articles",name="articles")
+     */
+    public function connexion()
+    {
+        $repository = $this->getDoctrine()->getManager()->getRepository(Article::class);
+        $ownArticles = $repository->findBy(
+            array('author' => 'Benoit LEFEVRE'), // Critere
+            array('created_date' => 'desc'),        // Tri
+            5,                              // Limite
+            0                               // Offset
+        );
+        $user = $this->getUser();
+        $fullUsername = $user->getFirstname().' '.$user->getLastname();
+        $otherArticles = $repository->otherArticles($fullUsername);
+
+        return $this->render('admin/articles.html.twig',[
+            'own_articles' => $ownArticles,
+            'other_articles' => $otherArticles,
+        ]);
+    }
+}
